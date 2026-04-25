8252d7075f8a4af29a5d99b26e121dc2a46fd927
pryusei <kypr9366@gmail.com>
2026-04-26 01:29:17 +0900

    fix(db): drizzle.config.ts で .env.local を読み込むよう修正

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,8 @@
+import { loadEnvConfig } from '@next/env'
 import { defineConfig } from 'drizzle-kit'
+
+// Next.js と同じ順序で .env.local などを読み込む
+loadEnvConfig(process.cwd())
 
 export default defineConfig({
   schema: './db/schema.ts',
